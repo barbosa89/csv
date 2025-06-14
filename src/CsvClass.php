f5a344dbc272d4bf271c6de67e379c6eacea8d83
Omar Barbosa <omar.barbosa@evertecinc.com>
2025-06-14 11:03:54 -0500

    init

---
+++ src/CsvClass.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Barbosa\Csv;
+
+class CsvClass {}
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Tests;
+namespace Barbosa\Csv\Tests;
 
 use PHPUnit\Framework\TestCase;
 
